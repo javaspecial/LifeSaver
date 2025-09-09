8ea9dae71260f38e23d8387e957e9f1a176b4f85
Shadath <shadathbsc@gmail.com>
2025-09-10 01:16:54 +0600

    Added connected spinner

--- app/src/main/java/com/peoples/shield/main/LandingActivity.java
+++ app/src/main/java/com/peoples/shield/main/LandingActivity.java
@@ -1,8 +1,13 @@
 package com.peoples.shield.main;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.peoples.shield.R;
 
 public class LandingActivity extends AppCompatActivity {
@@ -12,10 +17,20 @@ public class LandingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
 
-        findViewById(R.id.btn_connect).setOnClickListener(v -> {
-            Intent intent = new Intent(LandingActivity.this, MainActivity.class);
-            startActivity(intent);
-            finish();
+        Button btnConnect = findViewById(R.id.btn_connect);
+        btnConnect.setOnClickListener(v -> {
+            btnConnect.setText("Connecting...");
+            btnConnect.setEnabled(false);
+
+            Drawable progressDrawable = getResources().getDrawable(R.drawable.ic_spinner);
+            progressDrawable.setBounds(0, 0, 60, 60);
+            btnConnect.setCompoundDrawablesRelative(progressDrawable, null, null, null);
+
+            new Handler().postDelayed(() -> {
+                btnConnect.setCompoundDrawablesRelative(null, null, null, null);
+                startActivity(new Intent(LandingActivity.this, MainActivity.class));
+                finish();
+            }, 100);
         });
     }
 }
